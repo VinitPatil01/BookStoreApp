f6f0575fb2c28c2d5398fbf2efccb7204b390b23
VinitPatil01 <99319056+VinitPatil01@users.noreply.github.com>
2025-06-25 11:15:39 +0530

    Delete Frontend directory

--- Frontend/src/services/categoryService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const API = 'http://localhost:9090/api/categories';
-
-export const getAllCategories = () => axios.get(API);
-export const getCategoryById = (id) => axios.get(`${API}/${id}`);
-export const getCategoryByName = (name) => axios.get(`${API}/search?name=${name}`);
-export const createCategory = (category) => axios.post(API, category);
-export const updateCategory = (id, category) => axios.put(`${API}/${id}`, category);
-export const deleteCategory = (id) => axios.delete(`${API}/${id}`);
